40e1593162a3bec721e613d329ea2bc0fbbd2f67
Brad Chao <brad@brad.tw>
2016-08-30 14:10:37 +0800

    Brad19 v1

---
+++ src/tw/org/iii/java/Brad19.java
@@ -0,0 +1,16 @@
+package tw.org.iii.java;
+
+public class Brad19 {
+
+	public static void main(String[] args) {
+		int[] p = new int[6]; // p[0] = 0, .... p[5] = 0
+		for (int i=0; i<100; i++){
+			int point = (int)(Math.random()*6);	// 0 - 5
+			p[point]++;
+		}
+		for (int i=0; i<p.length; i++){
+			System.out.println((i+1) + "點出現"+p[i]+"次");
+		}
+	}
+
+}
