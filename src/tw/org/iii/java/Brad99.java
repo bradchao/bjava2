7c3c26d555857ba8616638ad8d7fa6fa95ee36b1
Brad Chao <brad@brad.tw>
2016-08-30 11:20:18 +0800

    2016-08-30 v1

---
+++ src/tw/org/iii/java/Brad99.java
@@ -0,0 +1,15 @@
+package tw.org.iii.java;
+
+public class Brad99 {
+
+	public static void main(String[] args) {
+		for (int j = 1; j <= 9; j++) {
+			for (int i = 2; i <= 5; i++) {
+				int result = i * j;
+				System.out.print(i + " x " + j + " = " + result + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
