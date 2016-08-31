df292d4efdecb1e0cda6ad57d808525b3460e165
Brad Chao <brad@brad.tw>
2016-08-31 15:53:15 +0800

    TWId v4

--- src/tw/org/iii/java/Brad25.java
+++ src/tw/org/iii/java/Brad25.java
@@ -3,11 +3,7 @@ package tw.org.iii.java;
 public class Brad25 {
 
 	public static void main(String[] args) {
-		String a = "A123456789";
-		System.out.println(a.substring(2, 3));
-		
-		
-		TWId id1 = new TWId("O123456789");
+		TWId id1 = new TWId("A123456789");
 		if (id1.isRight()){
 			System.out.println("OK");
 		}else{
