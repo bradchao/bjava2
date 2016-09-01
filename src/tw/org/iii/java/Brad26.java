872bfd773118c89c19655180ccadd9fff3c87233
Brad Chao <brad@brad.tw>
2016-09-01 10:04:55 +0800

    Brad26 v3

--- src/tw/org/iii/java/Brad26.java
+++ src/tw/org/iii/java/Brad26.java
@@ -7,22 +7,48 @@ public class Brad26 {
 	public static void main(String[] args) {
 		// 1. create Answer
 		String answer = createAnswer();
+		System.out.println(answer);
 		
 		// 2. start game
-		for (int i=0; i<3; i++){
-			// 3. Guess
-			String guess = JOptionPane.showInputDialog("Input");
-			
-			// 4. Check ?A?B
-			JOptionPane.showMessageDialog(null, "1A2B");
-		}
+//		for (int i=0; i<3; i++){
+//			// 3. Guess
+//			String guess = JOptionPane.showInputDialog("Input");
+//			
+//			// 4. Check ?A?B
+//			String result = checkAB(answer, guess);
+//			JOptionPane.showMessageDialog(null, 
+//					guess + ":" + result);
+//		}
 		
 		// 5. WINNER / ....
 	}
 
 	static String createAnswer(){
-		return "172";
+		// 洗牌
+		int[] poker = new int[4];
+		for (int i=0; i<poker.length; i++){
+			int temp;
+			// 檢查機制
+			boolean isRepeat;
+			do {
+				temp = (int)(Math.random()*10);
+				isRepeat = false;
+				for (int j=0; j<i; j++){
+					if (temp == poker[j]){
+						isRepeat = true;
+						break;
+					}
+				}
+			}while(isRepeat);
+			poker[i] = temp;
+		}
+		// poker[0], [1], [2]
+		
+		
+		return "" + poker[0] + poker[1] + poker[2] + poker[3];
 	}
-	
+	static String checkAB(String a, String g){
+		return "1A1B";
+	}
 	
 }
