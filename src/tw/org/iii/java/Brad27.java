c7b3a2702a1298c790f1917d66e43dafaa0579b0
Brad Chao <brad@brad.tw>
2016-09-01 10:50:44 +0800

    Brad271 v3

--- src/tw/org/iii/java/Brad27.java
+++ src/tw/org/iii/java/Brad27.java
@@ -2,11 +2,14 @@ package tw.org.iii.java;
 
 public class Brad27 {
 	public static void main(String[] args) {
-
+		Brad271 obj1 = new Brad271();
+		obj1.m1();
+		Brad272 obj2 = new Brad272();
+		obj2.m1();
 	}
 }
 class Brad271 {
-	
+	void m1(){System.out.println("Brad271:m1()");}
 }
 class Brad272 extends Brad271 {
 	
