64320ceeb71d6d9f0c383ff97cf7b976a6393fdf
Brad Chao <brad@brad.tw>
2016-09-01 12:00:05 +0800

    Brad28 v2

--- src/tw/org/iii/java/Brad28.java
+++ src/tw/org/iii/java/Brad28.java
@@ -1,10 +1,17 @@
 package tw.org.iii.java;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Brad28 extends JFrame {
+	private JButton open, save, exit;
+	
 	public Brad28(){
 		super("My Window App");
+		
+		open = new JButton("Open");
+		save = new JButton("Save");
+		exit = new JButton("Exit");
 		
 		setSize(640, 480);
 		setVisible(true);
