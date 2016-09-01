e8d444b1c13ff576d018a15a1a3b105ea8ba1a67
Brad Chao <brad@brad.tw>
2016-09-01 09:49:47 +0800

    2016-09-01-02

--- src/tw/org/iii/java/Brad26.java
+++ src/tw/org/iii/java/Brad26.java
@@ -1,10 +1,28 @@
 package tw.org.iii.java;
 
+import javax.swing.JOptionPane;
+
 public class Brad26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 1. create Answer
+		String answer = createAnswer();
+		
+		// 2. start game
+		for (int i=0; i<3; i++){
+			// 3. Guess
+			String guess = JOptionPane.showInputDialog("Input");
+			
+			// 4. Check ?A?B
+			JOptionPane.showMessageDialog(null, "1A2B");
+		}
+		
+		// 5. WINNER / ....
 	}
 
+	static String createAnswer(){
+		return "172";
+	}
+	
+	
 }
