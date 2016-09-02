85bb0b1052d4520face2cba4b79288f930f4a6e4
Brad Chao <brad@brad.tw>
2016-09-02 10:06:28 +0800

    Brad28 v4

--- src/tw/org/iii/java/Brad28.java
+++ src/tw/org/iii/java/Brad28.java
@@ -1,11 +1,13 @@
 package tw.org.iii.java;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Brad28 extends JFrame {
+public class Brad28 extends JFrame implements ActionListener{
 	private JButton open, save, exit;
 	
 	public Brad28(){
@@ -18,12 +20,18 @@ public class Brad28 extends JFrame {
 		exit = new JButton("Exit");
 		
 		add(open); add(save); add(exit);
+		open.addActionListener(this);
 		
 		setSize(640, 480);
 		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
 		new Brad28();
 	}
-
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("OK");
+	}
 }
