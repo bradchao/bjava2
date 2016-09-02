8aed4261045e6d0dff4ede2a84e52226fa5d37f2
Brad Chao <brad@brad.tw>
2016-09-02 10:54:17 +0800

    Brad30 v1

---
+++ src/tw/org/iii/java/Brad30.java
@@ -0,0 +1,14 @@
+package tw.org.iii.java;
+public class Brad30 {
+	public static void main(String[] args) {
+	
+	}
+}
+class Brad301 {
+	void m1(){}
+	void m2(){}
+}
+abstract class Brad302 {
+	void m1(){}
+	abstract void m2();
+}
