7e781eaa41d04e744feebfa3b1465900961575ea
Brad Chao <brad@brad.tw>
2016-09-02 09:34:38 +0800

    Brad29 V3

--- src/tw/org/iii/java/Brad29.java
+++ src/tw/org/iii/java/Brad29.java
@@ -2,7 +2,11 @@ package tw.org.iii.java;
 
 public class Brad29 {
 	public static void main(String[] args) {
-
+		Brad292 obj1 = new Brad292();
+		Brad293 obj2 = new Brad293();
+		
+		Brad294 obj3 = new Brad294();
+		obj3.m3(obj2);
 	}
 }
 interface Brad291 {
@@ -10,6 +14,15 @@ interface Brad291 {
 	void m2();
 }
 class Brad292 implements Brad291{
-	public void m1(){}
-	public void m2(){}
+	public void m1(){System.out.println("Brad292:m1()");}
+	public void m2(){System.out.println("Brad292:m2()");}
+}
+class Brad293 implements Brad291{
+	public void m1(){System.out.println("Brad293:m1()");}
+	public void m2(){System.out.println("Brad293:m2()");}
+}
+class Brad294 {
+	void m3(Brad291 b){
+		b.m1();
+	}
 }
