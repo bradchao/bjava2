5b6c8d9014f17d283ae01f68e15a652beb74533d
Brad Chao <brad@brad.tw>
2016-09-02 15:07:50 +0800

    Brad34 v2

--- src/tw/org/iii/java/Brad34.java
+++ src/tw/org/iii/java/Brad34.java
@@ -1,14 +1,30 @@
 package tw.org.iii.java;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Brad34 {
 	public static void main(String[] brad){
-		File file1 = new File("c:/king");
-		if (file1.exists()){
+		File file1 = new File("c:/king/file1");
+		if (file1.isFile()){
 			System.out.println("OK");
 		}else{
-			System.out.println("XX");
+			try{
+				if (file1.createNewFile()){
+					System.out.println("Create OK");
+				}else{
+					System.out.println("Create Fail");
+				}
+			}catch(IOException ie){
+				System.out.println();
+			}
 		}
+		
+		File file2 = new File("c:/test/dir1/dir2/dir3/dir4");
+		if (!file2.isDirectory()){
+			file2.mkdirs();
+		}
+		
+		
 	}
 }
