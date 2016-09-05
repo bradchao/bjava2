7be23c7ee9711f9a7b476600e4031fd1cd80702e
Brad Chao <brad@brad.tw>
2016-09-05 13:38:33 +0800

    Brad39 v2

--- src/tw/org/iii/java/Brad39.java
+++ src/tw/org/iii/java/Brad39.java
@@ -1,8 +1,24 @@
 package tw.org.iii.java;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Brad39 {
 	public static void main(String[] args) {
-
+		Student s1 = new Student(76, 54, 43);
+		System.out.println(s1.getScore());
+		System.out.println(s1.getAvg());
+		try {
+			ObjectOutputStream oout =
+				new ObjectOutputStream(
+					new FileOutputStream("dir1/s.dat"));
+			oout.writeObject(s1);
+			oout.flush();
+			oout.close();
+		} catch (IOException e) {
+			System.out.println("Exception: " + e.toString());
+		}
 	}
 }
 class Student {
