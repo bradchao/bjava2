71efcb025c6e7af136335b1b48ab185ace59c23c
Brad Chao <brad@brad.tw>
2016-09-05 15:52:03 +0800

    MyPainter v3

--- src/tw/org/iii/java/Drawer.java
+++ src/tw/org/iii/java/Drawer.java
@@ -1,7 +1,21 @@
 package tw.org.iii.java;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 public class Drawer extends JPanel {
-
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setColor(Color.red);
+		g2d.setStroke(new BasicStroke(4));
+		g2d.drawLine(0, 0, 100, 100);
+		
+	}
 }
