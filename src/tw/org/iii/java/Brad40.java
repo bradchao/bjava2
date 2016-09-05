28487a365c8902457a4b634c1fbc4c163ea612d6
Brad Chao <brad@brad.tw>
2016-09-05 13:54:22 +0800

    Brad40 v1

---
+++ src/tw/org/iii/java/Brad40.java
@@ -0,0 +1,25 @@
+package tw.org.iii.java;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Brad40 {
+
+	public static void main(String[] args) {
+		try {
+			ObjectInputStream oin = 
+				new ObjectInputStream(
+					new FileInputStream("dir1/s.dat"));
+			Object obj = oin.readObject();
+			Student s1 = (Student)obj;
+			oin.close();
+			System.out.println(s1.getScore());
+			System.out.println(s1.getAvg());
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+}
