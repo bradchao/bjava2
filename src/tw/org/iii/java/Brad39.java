7e0dd73ca94ae5a45304a98fd3467bd0d70c4497
Brad Chao <brad@brad.tw>
2016-09-05 14:03:00 +0800

    Brad39 v3

--- src/tw/org/iii/java/Brad39.java
+++ src/tw/org/iii/java/Brad39.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 public class Brad39 {
 	public static void main(String[] args) {
-		Student s1 = new Student(76, 54, 43);
+		Student s1 = new Student("Brad", 76, 54, 43);
 		System.out.println(s1.getScore());
 		System.out.println(s1.getAvg());
 		try {
@@ -24,7 +24,9 @@ public class Brad39 {
 }
 class Student implements Serializable{
 	int ch, eng, math;
-	Student(int ch, int eng, int math){
+	String name;
+	Student(String name, int ch, int eng, int math){
+		this.name = name;
 		this.ch = ch; this.eng = eng; this.math=math;
 	}
 	int getScore(){return ch+eng+math;}
