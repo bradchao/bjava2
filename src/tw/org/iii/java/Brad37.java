dd62267b3c7760db536f0880ed749338a4504a52
Brad Chao <brad@brad.tw>
2016-09-05 09:45:29 +0800

    Brad37 v3

--- src/tw/org/iii/java/Brad37.java
+++ src/tw/org/iii/java/Brad37.java
@@ -8,7 +8,7 @@ public class Brad37 {
 	public static void main(String[] args) {
 		try {
 			FileOutputStream fout =
-				new FileOutputStream("dir1/test1.txt");
+				new FileOutputStream("dir1/test1.txt", true);
 			fout.write("1234567\n7654321\nabcdefg".getBytes());
 			fout.flush();
 			fout.close();
