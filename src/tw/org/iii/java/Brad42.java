12ce3a4ef1b1ab06c70000a9392000e07546f821
Brad Chao <brad@brad.tw>
2016-09-05 15:16:12 +0800

    Brad42 43

--- src/tw/org/iii/java/Brad42.java
+++ src/tw/org/iii/java/Brad42.java
@@ -1,15 +1,26 @@
 package tw.org.iii.java;
 
-import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Brad42 {
 
 	public static void main(String[] args) {
-		HashSet set = new HashSet();
+		TreeSet set = new TreeSet();
 		while (set.size()<6){
 			set.add((int)(Math.random()*49+1));
 		}
 		System.out.println(set.toString());
+		Iterator it = set.iterator();
+		while (it.hasNext()){
+			Object obj = it.next();
+			System.out.println((Integer)obj);
+		}
+		System.out.println("----");
+		for(Object obj : set){
+			System.out.println((Integer)obj);
+		}
+		
 	}
 
 }
---
+++ src/tw/org/iii/java/Brad43.java
@@ -0,0 +1,19 @@
+package tw.org.iii.java;
+
+import java.util.LinkedList;
+
+public class Brad43 {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		//list.add(7,5);
+		System.out.println(list.toString());
+		
+		
+	}
+
+}
