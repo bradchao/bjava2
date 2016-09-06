90fe82b4a121572384a559148df9b4e5422c7523
Brad Chao <brad@brad.tw>
2016-09-06 11:08:28 +0800

    MyPainter v10

--- src/tw/org/iii/java/MyPainter.java
+++ src/tw/org/iii/java/MyPainter.java
@@ -2,35 +2,44 @@ package tw.org.iii.java;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class MyPainter extends JFrame{
+public class MyPainter extends JFrame {
 	private Drawer drawer;
-	private JButton clear, undo, redo;
-	
-	public MyPainter(){
+	private JButton clear, undo, redo, save;
+
+	public MyPainter() {
 		super("My Painter");
 		setLayout(new BorderLayout());
-		
+
 		clear = new JButton("Clear");
 		undo = new JButton("Undo");
 		redo = new JButton("Redo");
+		save = new JButton("Save");
 		JPanel top = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		top.add(clear);top.add(undo);top.add(redo);
+		top.add(clear);
+		top.add(undo);
+		top.add(redo);
+		top.add(save);
 		add(top, BorderLayout.NORTH);
-		
+
 		drawer = new Drawer();
 		add(drawer, BorderLayout.CENTER);
-		
+
 		setSize(1024, 768);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		clear.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -49,18 +58,52 @@ public class MyPainter extends JFrame{
 				redoDrawer();
 			}
 		});
-		
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				save2Drawer();
+			}
+		});
+
 	}
-	private void clearDrawer(){
+
+	private void clearDrawer() {
 		drawer.clear();
 	}
-	private void undoDrawer(){
+
+	private void undoDrawer() {
 		drawer.undo();
 	}
-	private void redoDrawer(){
+
+	private void redoDrawer() {
 		drawer.redo();
 	}
-	
+
+	private void saveDrawer() {
+		BufferedImage img = new BufferedImage(drawer.getWidth(), drawer.getHeight(), BufferedImage.TYPE_INT_RGB);
+		drawer.print(img.getGraphics()); // or: panel.printAll(...);
+		try {
+			ImageIO.write(img, "jpg", new File("dir1/drawer.jpg"));
+			System.out.println("Save OK");
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	private void save2Drawer() {
+		BufferedImage bi = 
+				new BufferedImage(drawer.getWidth(), drawer.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics g = bi.createGraphics();
+		drawer.paint(g); // this == JComponent
+		g.dispose();
+		try {
+			ImageIO.write(bi, "png", new File("dir1/test.png"));
+			System.out.println("Save2 OK");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
 	public static void main(String[] args) {
 		new MyPainter();
 	}
