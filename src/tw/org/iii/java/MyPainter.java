4c1bce342ef4e238d30fbb5a69534a57a81098d0
Brad Chao <brad@brad.tw>
2016-09-06 10:16:11 +0800

    MyPainter V6

--- src/tw/org/iii/java/MyPainter.java
+++ src/tw/org/iii/java/MyPainter.java
@@ -1,16 +1,24 @@
 package tw.org.iii.java;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MyPainter extends JFrame{
 	private Drawer drawer;
+	private JButton clear;
 	
 	public MyPainter(){
 		super("My Painter");
-	
 		setLayout(new BorderLayout());
+		
+		clear = new JButton("Clear");
+		JPanel top = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		top.add(clear);
+		add(top, BorderLayout.NORTH);
 		
 		drawer = new Drawer();
 		add(drawer, BorderLayout.CENTER);
