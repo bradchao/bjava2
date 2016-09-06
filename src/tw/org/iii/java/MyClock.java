f7fd5cc205b2e0aec48053ddf3e79447d9ce4ae5
Brad Chao <brad@brad.tw>
2016-09-06 16:01:00 +0800

    MyClock v3

--- src/tw/org/iii/java/MyClock.java
+++ src/tw/org/iii/java/MyClock.java
@@ -1,5 +1,6 @@
 package tw.org.iii.java;
 
+import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,6 +16,11 @@ public class MyClock extends JLabel {
 	private class MyTask extends TimerTask {
 		@Override
 		public void run() {
+			Calendar now = Calendar.getInstance();
+			int hh = now.get(Calendar.HOUR_OF_DAY);
+			int mm = now.get(Calendar.MINUTE);
+			int ss = now.get(Calendar.SECOND);
+			setText(hh + ":" + mm + ":" +ss);
 			
 		}
 	}
