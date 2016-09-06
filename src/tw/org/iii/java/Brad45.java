9588dcb7c514b040f6f7bb2942778eb5c160b62a
Brad Chao <brad@brad.tw>
2016-09-06 13:53:39 +0800

    Brad45 v4

--- src/tw/org/iii/java/Brad45.java
+++ src/tw/org/iii/java/Brad45.java
@@ -4,8 +4,11 @@ public class Brad45 {
 	public static void main(String[] args) {
 		Brad451 obj1 = new Brad451("A");
 		Brad451 obj2 = new Brad451("B");
+		Brad452 obj3 = new Brad452("C");
+		Thread t1 = new Thread(obj3);
 		obj1.start();
 		obj2.start();
+		t1.start();
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
@@ -20,6 +23,21 @@ public class Brad45 {
 class Brad451 extends Thread {
 	String name;
 	Brad451(String name){this.name = name;}
+	@Override
+	public void run() {
+		for (int i=0; i<10; i++){
+			System.out.println(name + ":" + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
+}
+class Brad452 implements Runnable {
+	String name;
+	Brad452(String name){this.name = name;}
 	@Override
 	public void run() {
 		for (int i=0; i<10; i++){
