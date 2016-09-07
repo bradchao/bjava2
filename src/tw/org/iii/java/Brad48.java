efda8027a1ca284f8643a5d566872c239dcfacb1
Brad Chao <brad@brad.tw>
2016-09-07 11:12:13 +0800

    Brad48 v1

---
+++ src/tw/org/iii/java/Brad48.java
@@ -0,0 +1,18 @@
+package tw.org.iii.java;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Brad48 {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress ip = 
+				InetAddress.getByName("www.iii.org.tw");
+			System.out.println(ip.getHostAddress());
+		} catch (UnknownHostException e) {
+			System.out.println("UnknownHost");
+		}
+	}
+
+}
