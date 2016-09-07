c30805cb6f077160daf2a452c65562ce558caf1d
Brad Chao <brad@brad.tw>
2016-09-07 09:30:01 +0800

    Brad47 v3

--- src/tw/org/iii/java/Brad47.java
+++ src/tw/org/iii/java/Brad47.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -22,13 +24,25 @@ public class Brad47 extends JFrame{
 	}
 	
 	private class MyGame extends JPanel {
+		private Timer timer;
+		private int ballX, ballY;
+		MyGame(){
+			timer = new Timer();
+			
+		}
+		private class BallTask extends TimerTask {
+			@Override
+			public void run() {
+				ballX += 10; ballY += 10;
+			}
+		}
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			Graphics2D g2d = (Graphics2D)g;
 			
 			g2d.setColor(Color.red);
-			g2d.fillOval(100, 100, 30, 30);
+			g2d.fillOval(ballX, ballY, 30, 30);
 		}
 	}
 	
