e767213591e9b5c7475b5702410bce1f85954ddf
Brad Chao <brad@brad.tw>
2016-09-07 09:25:54 +0800

    Brad47 v2

--- src/tw/org/iii/java/Brad47.java
+++ src/tw/org/iii/java/Brad47.java
@@ -1,6 +1,9 @@
 package tw.org.iii.java;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,7 +22,14 @@ public class Brad47 extends JFrame{
 	}
 	
 	private class MyGame extends JPanel {
-		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D)g;
+			
+			g2d.setColor(Color.red);
+			g2d.fillOval(100, 100, 30, 30);
+		}
 	}
 	
 	public static void main(String[] args) {
