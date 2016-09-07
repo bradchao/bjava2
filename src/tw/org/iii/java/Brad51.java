ebe6fcb3bdbc75f087fec9ac1d687525dd9ed7f7
Brad Chao <brad@brad.tw>
2016-09-07 15:18:10 +0800

    Brad51 v5

--- src/tw/org/iii/java/Brad51.java
+++ src/tw/org/iii/java/Brad51.java
@@ -1,5 +1,6 @@
 package tw.org.iii.java;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -9,25 +10,26 @@ import java.net.Socket;
 public class Brad51 {
 
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 		try {
+			File sendFile = new File("dir1/coffee.jpg");
+			byte[] buf = new byte[(int)sendFile.length()];
+			FileInputStream fin = 
+					new FileInputStream(sendFile);
+			fin.read(buf);
+			fin.close();
+			
+			
 			Socket socket = new Socket(
 				InetAddress.getByName("10.1.6.65"), 9999);
 			
-			FileInputStream fin = 
-				new FileInputStream("dir1/coffee.jpg");
 			OutputStream out = socket.getOutputStream();
-
-			int b;
-			while ( (b = fin.read()) != -1){
-				out.write(b);
-			}
-			
+			out.write(buf);
 			out.flush();
 			out.close();
 			
-			fin.close();
-			
 			socket.close();
+			System.out.println(System.currentTimeMillis()-start);
 			System.out.println("Send OK!");
 		} catch (IOException e) {
 			System.out.println(e.toString());
