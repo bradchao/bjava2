fe952d11206d66270b2e0a6d96c7c25aad868af3
Brad Chao <brad@brad.tw>
2016-09-08 10:51:10 +0800

    Brad54 v2

--- src/tw/org/iii/java/Brad54.java
+++ src/tw/org/iii/java/Brad54.java
@@ -13,14 +13,14 @@ public class Brad54 {
 
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("http://a.ecimg.tw/pic/v1/data/item/201609/D/R/A/G/6/S/DRAG6S-19007D0LW000_57d0c1eba36c7.jpg");
+			URL url = new URL("http://pdfmyurl.com/?url=http://www.gamer.com.tw");
 			HttpURLConnection conn = 
 					(HttpURLConnection)url.openConnection();
 			conn.connect();
 
 			InputStream in = conn.getInputStream();
 			FileOutputStream fout = 
-				new FileOutputStream("dir1/brad2.jpg");
+				new FileOutputStream("dir1/gamer.pdf");
 			byte[] buf = new byte[4096]; int len;
 			while ( (len = in.read(buf)) !=-1){
 				fout.write(buf, 0, len);
